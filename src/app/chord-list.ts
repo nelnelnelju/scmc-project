e91f32e8ffac201940ea29d0ac57ddc61be8871f
Junel Fronda <junel.fronda@dxc.com>
2025-08-14 07:29:01 +0800

    add new songs

--- src/app/chord-list.ts
+++ src/app/chord-list.ts
@@ -8,7 +8,27 @@ export const SONGLIST = [
     title: 'Hindi Ka Nagkulang',
   },
   {
-    link: 'https://docs.google.com/document/d/1ZemSNGcdHH5DAuW5l1CZm4UW_L2h9ga2/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    link: 'https://docs.google.com/document/d/1ZemSNGcdHH5DAuW5l1CZm4UW_L2h9ga2/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
     title: 'Ikaw Sa Buhay Ko',
+  },
+  {
+    link: 'https://docs.google.com/document/d/1tfQCjn8HshiAsGf9KtFtd8z5vPEUKWaQ/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    title: 'Katulad ng mga Agila',
+  },
+  {
+    link: 'https://docs.google.com/document/d/1k9uF06bjxMXkZ26LEGCoBG6Jvg7Y9_te/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    title: 'Sa Biyaya',
+  },
+  {
+    link: 'https://docs.google.com/document/d/1Mg01DR9cJaLodYEoiWycFrpFgSmbd1Qp/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    title: 'Salamat Panginoon',
+  },
+  {
+    link: 'https://docs.google.com/document/d/1i1XMCGeli-_6msTHJZ8EO6jTL5sAMpK9/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    title: 'Sapat Na Higit Pa',
+  },
+  {
+    link: 'https://docs.google.com/document/d/1ISTNjqv7QHXN1MhacgMqxLUrdhEYqDpi/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    title: 'Walang Mahirap Sayo',
   },
 ];
