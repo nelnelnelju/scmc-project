7ec21ad1378bcf650cbd154bedea53ffe4e2b79a
Junel Fronda <junel.fronda@dxc.com>
2025-08-14 19:43:20 +0800

    Add redirection to home when clicking header text

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.html',
-  styleUrl: './header.scss'
+  styleUrl: './header.scss',
 })
 export class Header {
+  private readonly router = inject(Router);
 
+  goToHome() {
+    this.router.navigate(['home']);
+  }
 }
