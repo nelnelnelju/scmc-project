41e8b98860e8e89f735ece8d5cbaed03134bbe7a
Junel Fronda <junel.fronda@dxc.com>
2025-08-28 20:50:39 +0800

    Add songs for aug 31

--- src/app/song-list.ts
+++ src/app/song-list.ts
@@ -1,68 +1,106 @@
 export const SONGLIST = [
   {
     title: 'Dakilang Katapatan',
-    viewLink: 'https://docs.google.com/document/d/16q21UxnFLS9gEqTorPP9v1FMrfloH-L6/preview?usp=sharing&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/16q21UxnFLS9gEqTorPP9v1FMrfloH-L6/edit?usp=sharing&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=RoHPsqQf99k'
+    viewLink:
+      'https://docs.google.com/document/d/16q21UxnFLS9gEqTorPP9v1FMrfloH-L6/preview?usp=sharing&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/16q21UxnFLS9gEqTorPP9v1FMrfloH-L6/edit?usp=sharing&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=RoHPsqQf99k',
   },
   {
     title: 'Hindi Ka Nagkulang',
-    viewLink: 'https://docs.google.com/document/d/1WvV_ueo-FizB7r5rn3Ry-pD0S1TdDKWP/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/1WvV_ueo-FizB7r5rn3Ry-pD0S1TdDKWP/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=CvMxmCfH2YY'
+    viewLink:
+      'https://docs.google.com/document/d/1WvV_ueo-FizB7r5rn3Ry-pD0S1TdDKWP/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1WvV_ueo-FizB7r5rn3Ry-pD0S1TdDKWP/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=CvMxmCfH2YY',
   },
   {
     title: 'Sapat Na Higit Pa / Araw-Araw',
-    viewLink: 'https://docs.google.com/document/d/1i1XMCGeli-_6msTHJZ8EO6jTL5sAMpK9/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/1i1XMCGeli-_6msTHJZ8EO6jTL5sAMpK9/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=OIZxUGRmFK4'
+    viewLink:
+      'https://docs.google.com/document/d/1i1XMCGeli-_6msTHJZ8EO6jTL5sAMpK9/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1i1XMCGeli-_6msTHJZ8EO6jTL5sAMpK9/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=OIZxUGRmFK4',
   },
   {
     title: 'Walang Mahirap Sayo',
-    viewLink: 'https://docs.google.com/document/d/1ISTNjqv7QHXN1MhacgMqxLUrdhEYqDpi/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/1ISTNjqv7QHXN1MhacgMqxLUrdhEYqDpi/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=1cHz1PlBsMM'
+    viewLink:
+      'https://docs.google.com/document/d/1ISTNjqv7QHXN1MhacgMqxLUrdhEYqDpi/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1ISTNjqv7QHXN1MhacgMqxLUrdhEYqDpi/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=1cHz1PlBsMM',
   },
   {
     title: 'Ikaw Sa Buhay Ko',
-    viewLink: 'https://docs.google.com/document/d/1ZemSNGcdHH5DAuW5l1CZm4UW_L2h9ga2/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/1ZemSNGcdHH5DAuW5l1CZm4UW_L2h9ga2/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=dPOdkJCkV2k'
+    viewLink:
+      'https://docs.google.com/document/d/1ZemSNGcdHH5DAuW5l1CZm4UW_L2h9ga2/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1ZemSNGcdHH5DAuW5l1CZm4UW_L2h9ga2/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=dPOdkJCkV2k',
   },
   {
     title: 'Katulad ng mga Agila',
-    viewLink: 'https://docs.google.com/document/d/1tfQCjn8HshiAsGf9KtFtd8z5vPEUKWaQ/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/1tfQCjn8HshiAsGf9KtFtd8z5vPEUKWaQ/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=b41TeZCDVWM'
+    viewLink:
+      'https://docs.google.com/document/d/1tfQCjn8HshiAsGf9KtFtd8z5vPEUKWaQ/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1tfQCjn8HshiAsGf9KtFtd8z5vPEUKWaQ/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=b41TeZCDVWM',
   },
   {
     title: 'Sa Biyaya',
-    viewLink: 'https://docs.google.com/document/d/1k9uF06bjxMXkZ26LEGCoBG6Jvg7Y9_te/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/1k9uF06bjxMXkZ26LEGCoBG6Jvg7Y9_te/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=xu5rzYSkSRI'
+    viewLink:
+      'https://docs.google.com/document/d/1k9uF06bjxMXkZ26LEGCoBG6Jvg7Y9_te/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1k9uF06bjxMXkZ26LEGCoBG6Jvg7Y9_te/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=xu5rzYSkSRI',
   },
   {
     title: 'Salamat Panginoon',
-    viewLink: 'https://docs.google.com/document/d/1Mg01DR9cJaLodYEoiWycFrpFgSmbd1Qp/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/1Mg01DR9cJaLodYEoiWycFrpFgSmbd1Qp/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=NDdLDUX_ekQ'
+    viewLink:
+      'https://docs.google.com/document/d/1Mg01DR9cJaLodYEoiWycFrpFgSmbd1Qp/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1Mg01DR9cJaLodYEoiWycFrpFgSmbd1Qp/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=NDdLDUX_ekQ',
   },
   {
     title: 'Salamat Salamat',
-    viewLink: 'https://docs.google.com/document/d/1MqF8DNSuLBLZv2XrfD4KVFRiBklSDDUR/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/1MqF8DNSuLBLZv2XrfD4KVFRiBklSDDUR/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=Z_2sCkjESDg'
+    viewLink:
+      'https://docs.google.com/document/d/1MqF8DNSuLBLZv2XrfD4KVFRiBklSDDUR/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1MqF8DNSuLBLZv2XrfD4KVFRiBklSDDUR/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=Z_2sCkjESDg',
   },
   {
     title: 'What A Beautiful Name',
-    viewLink: 'https://docs.google.com/document/d/1IPf48lHUHFWUPCCOyy1NbvMeUosGJx_O/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/1IPf48lHUHFWUPCCOyy1NbvMeUosGJx_O/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=nQWFzMvCfLE'
+    viewLink:
+      'https://docs.google.com/document/d/1IPf48lHUHFWUPCCOyy1NbvMeUosGJx_O/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1IPf48lHUHFWUPCCOyy1NbvMeUosGJx_O/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=nQWFzMvCfLE',
   },
   {
     title: 'Oceans',
-    viewLink: 'https://docs.google.com/document/d/1xn-R65xZLu7-b12TE0en7fL8ljVTdB7b/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    downloadLink: 'https://docs.google.com/document/d/1xn-R65xZLu7-b12TE0en7fL8ljVTdB7b/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
-    youtubeLink: 'https://www.youtube.com/watch?v=3TY1RhTuZhE'
+    viewLink:
+      'https://docs.google.com/document/d/1xn-R65xZLu7-b12TE0en7fL8ljVTdB7b/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1xn-R65xZLu7-b12TE0en7fL8ljVTdB7b/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=3TY1RhTuZhE',
+  },
+  {
+    title: 'Jesus My Everything/ Worthy Of It All',
+    viewLink:
+      'https://docs.google.com/document/d/1aDeNFH56RRXY0SP1aksfUyKV1VsvbfEF/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1aDeNFH56RRXY0SP1aksfUyKV1VsvbfEF/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=DDF9T4_4FLM',
+  },
+  {
+    title: 'Blessed Be Your Name',
+    viewLink:
+      'https://docs.google.com/document/d/1i7f1m1a3r3FPeO2TyrrfjpXfTnS62ykz/preview?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    downloadLink:
+      'https://docs.google.com/document/d/1i7f1m1a3r3FPeO2TyrrfjpXfTnS62ykz/edit?usp=drive_link&ouid=113146195665386428162&rtpof=true&sd=true',
+    youtubeLink: 'https://www.youtube.com/watch?v=6AB3ku_c7Dw',
   },
 ];
